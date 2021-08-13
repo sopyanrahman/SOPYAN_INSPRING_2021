c545c0654c07f99204a650ac01774f71b619c9f6
sopyanrahman <88863090+sopyanrahman@users.noreply.github.com>
2021-08-12 20:11:54 -1200

    Add files via upload

---
+++ slide4.js
@@ -0,0 +1,9 @@
+(function(){var loadHandler=window['sl_{E5EA4959-67EB-437B-A5FE-598F39BF46BE}'];loadHandler&&loadHandler(3, '<div id="spr0_93efbd"><div id="spr1_93efbd" class="kern slide"><img id="img3_93efbd" src="data/img2.png" width="720px" height="405px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_93efbd" class="kern slide"><div id="spr3_93efbd"><img id="img0_93efbd" src="data/img0.png" width="721.5" height="406.5" alt="Z:\\03.png" style="left:-0.563px;top:-0.563px;"/></div><div id="svg0_93efbd" style="left:269.25px;top:9.75px;"><svg width="380" height="87" viewBox="-0.375 -0.375 380 87"><path fill="#008000" stroke="#ffff00" stroke-width="0.8" stroke-linecap="butt" stroke-linejoin="round" d="M0.375,0.375 h378 v84.82 h-378 Z"/></svg></div><div id="spr4_93efbd" style="left:314.25px;top:20.25px;"><img id="img1_93efbd" src="data/img7.png" width="293.25" height="72.75" alt="Syarat Terjadinya \
+Interaksi Antarruang"/></div><div id="svg1_93efbd" style="left:34px;top:104.5px;"><svg width="574" height="283" viewBox="-1 -1 574 283"><path fill="#002060" stroke="#ffffff" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h570 v278.695 h-570 Z"/></svg></div><div id="spr5_93efbd" style="left:99.75px;top:116.25px;"><img id="img2_93efbd" src="data/img8.png" width="447" height="264.75" alt="COMPLEMENTARITY \
+(Saling Melengkapi)\
+\
+2.  INTERVENING OPPURTUNITY\
+(Kesempatan Antara)\
+\
+3.  TRANSFER ABILITY \
+(Kemudahan Transfer)"/></div></div></div>');})();
---
+++ slide5.js
@@ -0,0 +1,11 @@
+(function(){var loadHandler=window['sl_{E5EA4959-67EB-437B-A5FE-598F39BF46BE}'];loadHandler&&loadHandler(4, '<div id="spr0_93f0c6"><div id="spr1_93f0c6" class="kern slide"><img id="img3_93f0c6" src="data/img2.png" width="720px" height="405px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_93f0c6" class="kern slide"><div id="spr3_93f0c6" style="top:-1.5px;"><img id="img0_93f0c6" src="data/img0.png" width="721.5" height="406.5" alt="Z:\\03.png" style="left:-0.563px;top:-0.563px;"/></div><div id="svg0_93f0c6" style="left:383.25px;top:2.93px;"><svg width="266" height="87" viewBox="-0.375 -0.375 266 87"><path fill="#008000" stroke="#ffff00" stroke-width="0.8" stroke-linecap="butt" stroke-linejoin="round" d="M0.375,0.375 h264 v84.82 h-264 Z"/></svg></div><div id="spr4_93f0c6" style="left:401.25px;top:13.5px;"><img id="img1_93f0c6" src="data/img9.png" width="234" height="72.75" alt="Bentuk Interaksi Antarruang"/></div><div id="svg1_93f0c6" style="left:28px;top:94.417px;"><svg width="532" height="303" viewBox="-1 -1 532 303"><path fill="#002060" stroke="#ffffff" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h528 v298.083 h-528 Z"/></svg></div><div id="spr5_93f0c6" style="left:66.75px;top:106.5px;"><img id="img2_93f0c6" src="data/img10.png" width="456.75" height="280.5" alt="Mobilitas Penduduk\
+Pergerakan/ Perpindahan Manusia, \
+Misalnya: Transmigrasi, Emigrasi, Imigrasi, Urbanisasi, dll\
+\
+Komunikasi\
+Perpindahan Ide/ Gagasan dan Infromasi,\
+Misalnya: Tayangan TV, Membaca Berita, Internet, dll\
+\
+Transportasi\
+Perpindahan Barang atau Energi, \
+Misalnya: Pengangkutan Barang, Pegudangan, dll "/></div></div></div>');})();
